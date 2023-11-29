53a588f0b10d9cbefb1c73ea8ddc026b8d50602f
Mateo Rios <mateoriosq@gmail.com>
2023-11-28 21:38:05 -0500

    Change google/flan-t5-xl to google/flan-t5-small

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import base64
 from io import BytesIO
 
 # Load model
-tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-xl")
-model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-xl").to("cuda")
+tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-small")
+model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-small").to("cuda")
 
 pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", revision="fp16", torch_dtype=torch.float16)
 pipe.to("cuda")
